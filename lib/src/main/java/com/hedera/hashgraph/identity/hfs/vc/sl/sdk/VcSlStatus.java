c82098e57caa80b99ab4ab41e3bc7ee109aceac4
Vijay Shiyani <vshiyani@meeco.me>
2022-04-07 10:52:58 +1000

    refactor status list.

--- lib/src/main/java/com/hedera/hashgraph/identity/hfs/vc/sl/sdk/VcSlStatus.java
+++ lib/src/main/java/com/hedera/hashgraph/identity/hfs/vc/sl/sdk/VcSlStatus.java
@@ -1,18 +1,8 @@
 package com.hedera.hashgraph.identity.hfs.vc.sl.sdk;
 
 public enum VcSlStatus {
-    ACTIVE(0),
-    RESUMED(1),
-    SUSPENDED(2),
-    REVOKED(3);
-
-    private final int value;
-
-    VcSlStatus(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return this.value;
-    }
+    ACTIVE,
+    RESUMED,
+    SUSPENDED,
+    REVOKED
 }
